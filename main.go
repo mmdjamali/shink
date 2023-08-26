874963b39a1efc397c2842649b54f67dea87efdc
MmD <mmdj27634@gmail.com>
2023-08-26 04:07:49 -0700

    learn : about interface and struct types

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //! learning Goroutines
 
@@ -28,22 +26,72 @@ import (
 
 //! learning Channels
 
+// func main() {
+// 	channel := make(chan string)
+// 	output := make(chan string)
+
+// 	go func() {
+// 		s := <-channel
+// 		s = s + "GO!"
+// 		output <- s
+// 	}()
+
+// 	go func() {
+// 		s := "Hello, "
+// 		channel <- s
+// 	}()
+
+// 	finalString := <-output
+
+// 	fmt.Println("This is created by channels:", finalString)
+// }
+
+//! learning Go's type system
+
+// type Describer interface {
+// 	describe() string
+// }
+
+// func printDescribe(d Describer) {
+// 	fmt.Println(d.describe())
+// }
+
+// type Person struct {
+// 	first_name string
+// 	last_name  string
+// 	age        int
+// }
+
+// func (p Person) name() string {
+// 	return p.first_name + " " + p.last_name
+// }
+
+// func (p Person) describe() string {
+// 	return fmt.Sprintf("%s is %d years old", p.name(), p.age)
+// }
+
+// func main() {
+// 	mmd := Person{
+// 		first_name: "Mohammad",
+// 		last_name:  "Jamali",
+// 		age:        19,
+// 	}
+
+// 	printDescribe(mmd)
+// }
+
+//! learning Go's generics
+
+func sumNumbers[N int | float64](numbers ...N) N {
+	var total N
+	for i := range numbers {
+		total += numbers[i]
+	}
+
+	return total
+}
+
 func main() {
-	channel := make(chan string)
-	output := make(chan string)
-
-	go func() {
-		s := <-channel
-		s = s + "GO!"
-		output <- s
-	}()
-
-	go func() {
-		s := "Hello, "
-		channel <- s
-	}()
-
-	finalString := <-output
-
-	fmt.Println("This is created by channels:", finalString)
+	fmt.Println(sumNumbers[int](1, 5, 6, 8))
+	fmt.Println(sumNumbers(1.5, 5.2, 7.2, 8.0))
 }
