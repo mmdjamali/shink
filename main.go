5f213a23617e3a72c65b30529118ded7086e3a7e
MmD <mmdj27634@gmail.com>
2023-09-03 12:51:21 -0700

    feat : add views and public stuff

--- main.go
+++ main.go
@@ -1,97 +1,30 @@
 package main
 
-import "fmt"
-
-//! learning Goroutines
-
-// func do_something() {
-// 	fmt.Println("SLOW : start")
-// 	time.Sleep(1 * time.Second)
-// 	fmt.Println("SLOW : finish")
-// }
-
-// func main() {
-// 	fmt.Println("MAIN : start")
-
-// 	go do_something()
-
-// 	fmt.Println("MAIN : continue")
-
-// 	time.Sleep(500 * time.Millisecond)
-
-// 	fmt.Println("MAIN : finished")
-
-// 	time.Sleep(1 * time.Second)
-// }
-
-//! learning Channels
-
-// func main() {
-// 	channel := make(chan string)
-// 	output := make(chan string)
-
-// 	go func() {
-// 		s := <-channel
-// 		s = s + "GO!"
-// 		output <- s
-// 	}()
-
-// 	go func() {
-// 		s := "Hello, "
-// 		channel <- s
-// 	}()
-
-// 	finalString := <-output
-
-// 	fmt.Println("This is created by channels:", finalString)
-// }
-
-//! learning Go's type system
-
-// type Describer interface {
-// 	describe() string
-// }
-
-// func printDescribe(d Describer) {
-// 	fmt.Println(d.describe())
-// }
-
-// type Person struct {
-// 	first_name string
-// 	last_name  string
-// 	age        int
-// }
-
-// func (p Person) name() string {
-// 	return p.first_name + " " + p.last_name
-// }
-
-// func (p Person) describe() string {
-// 	return fmt.Sprintf("%s is %d years old", p.name(), p.age)
-// }
-
-// func main() {
-// 	mmd := Person{
-// 		first_name: "Mohammad",
-// 		last_name:  "Jamali",
-// 		age:        19,
-// 	}
-
-// 	printDescribe(mmd)
-// }
-
-//! learning Go's generics
-
-func sumNumbers[N int | float64](numbers ...N) N {
-	var total N
-	for i := range numbers {
-		total += numbers[i]
-	}
-
-	return total
-}
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
-	fmt.Println(sumNumbers[int](1, 5, 6, 8))
-	fmt.Println(sumNumbers(1.5, 5.2, 7.2, 8.0))
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layouts/main",
+	})
+
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	app.Static("/public", "./public")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("main", fiber.Map{
+			"Title": "Shink - Free Link shortener",
+		})
+	})
+
+	app.Listen(":3001")
 }
---
+++ public/js/config.js
@@ -0,0 +1,30 @@
+window.__unocss = {
+  rules: [],
+  shortcuts: {
+    // shortcuts to multiple utilities
+    btn: "py-2 px-4 font-semibold rounded-lg shadow-md",
+    "btn-green": "text-white bg-green-500 hover:bg-green-700",
+    // single utility alias
+    red: "text-red-100",
+  },
+  theme: {
+    colors: {
+      test: "#313131",
+      foreground: "rgb(var(--foreground))",
+      background: "rgb(var(--background))",
+      border: "rgb(var(--border))",
+      primary: {
+        DEFAULT: "rgb(var(--primary))",
+      },
+      success: {
+        DEFAULT: "rgb(var(--success))",
+      },
+      error: {
+        DEFAULT: "rgb(var(--error))",
+      },
+      warning: {
+        DEFAULT: "rgb(var(--warning))",
+      },
+    },
+  },
+};
