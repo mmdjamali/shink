6780fa91796679d6cf228859a31feeb13f1ea45e
MmD <mmdj27634@gmail.com>
2023-09-09 09:08:58 -0700

    add : utility for domain check

--- utils/index.go
+++ utils/index.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"fmt"
 	"net/mail"
 	"net/url"
+	"os"
+	"strings"
 )
 
 type CustomError struct {
@@ -21,4 +24,23 @@ func IsValidEmail(e string) bool {
 func IsValidURL(u string) bool {
 	_, err := url.ParseRequestURI(u)
 	return err == nil
+}
+
+func IsDomainDiffrent(url string) bool {
+	// basically this functions removes all the commonly found
+	// prefixes from URL such as http, https, www
+	// then checks of the remaining string is the DOMAIN itself
+	if url == os.Getenv("DOMAIN") {
+		return false
+	}
+	newURL := strings.Replace(url, "http://", "", 1)
+	newURL = strings.Replace(newURL, "https://", "", 1)
+	newURL = strings.Replace(newURL, "www.", "", 1)
+	newURL = strings.Split(newURL, "/")[0]
+	newURL = strings.Replace(newURL, " ", "", -1)
+
+	fmt.Println(newURL)
+	fmt.Println(os.Getenv("DOMAIN"))
+
+	return newURL != os.Getenv("DOMAIN")
 }
