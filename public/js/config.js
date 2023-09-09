7eafe5e3429182693c1e6287dd954ad6c9f82dfd
MmD <mmdj27634@gmail.com>
2023-09-09 09:10:50 -0700

    ref : remove some trash from config

--- public/js/config.js
+++ public/js/config.js
@@ -1,12 +1,4 @@
 window.__unocss = {
-  rules: [],
-  shortcuts: {
-    // shortcuts to multiple utilities
-    btn: "py-2 px-4 font-semibold rounded-lg shadow-md",
-    "btn-green": "text-white bg-green-500 hover:bg-green-700",
-    // single utility alias
-    red: "text-red-100",
-  },
   theme: {
     colors: {
       test: "#313131",
